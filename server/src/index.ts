ce8ec8417db7b7294d3bafa3acd3454fae24bc7e
dngtnv <kakalupin@gmail.com>
2024-11-19 11:39:56 +0700

    init commit

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000/");
+});
