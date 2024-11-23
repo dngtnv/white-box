44eb39aa27fe53372fa9f0ca493a3bb2961969c3
dngtnv <kakalupin@gmail.com>
2024-11-23 20:15:50 +0700

    change field name to createdAt

--- server/src/controllers/thought.controllers.ts
+++ server/src/controllers/thought.controllers.ts
@@ -17,7 +17,7 @@ export const postThoughts = async (req: Request, res: Response, next: NextFuncti
 
 export const getThoughts = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const thoughts = await Thought.find().sort({ timestamp: -1 }).limit(20);
+    const thoughts = await Thought.find().sort({ createdAt: -1 }).limit(20);
     res.status(200).json(thoughts);
   } catch (err: any) {
     if (!err.statusCode) {
