59b582ff9ef7be895325b6b30a3474c9251dfa91
dngtnv <kakalupin@gmail.com>
2024-12-03 11:39:02 +0700

    remove semicolon

--- client/src/components/StyleOptions.tsx
+++ client/src/components/StyleOptions.tsx
@@ -9,7 +9,7 @@ const gradients = {
   purple: "#dfc1f3",
   rose: "#ff8696",
   gradient1:
-    "linear-gradient(68.1deg, rgb(255, 200, 69) -0.3%, rgb(239, 69, 115) 100.2%);",
+    "linear-gradient(68.1deg, rgb(255, 200, 69) -0.3%, rgb(239, 69, 115) 100.2%)",
   gradient2:
     "linear-gradient(90deg, rgba(33,150,243,1) 0%, rgba(0,230,118,1) 50%, rgba(255,235,59,1) 100%)",
   gradient3:
