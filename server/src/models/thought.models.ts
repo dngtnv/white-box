f508baf837cc31c9a4078aba226ba88457a62fa5
dngtnv <kakalupin@gmail.com>
2025-02-09 22:50:32 +0700

    change TTL of content to 7 days

--- server/src/models/thought.models.ts
+++ server/src/models/thought.models.ts
@@ -12,7 +12,6 @@ const styleSchema = new Schema(
 const thoughtSchema = new Schema({
   content: { type: String, required: true, maxlength: 280 },
   styles: { type: styleSchema },
-  createdAt: { type: Date, default: Date.now, expires: "1d" }
 });
 
 const ThoughtModel = mongoose.model("Thought", thoughtSchema);
